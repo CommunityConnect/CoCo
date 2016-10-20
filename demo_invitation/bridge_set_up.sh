bb037ec860f1acf454ba98a6b1b30682ce869d03
coco <marta.antosik@tno.nl>
2016-10-20 10:25:38 +0200

    adjust MAC change rule

--- demo_invitation/bridge_set_up.sh
+++ demo_invitation/bridge_set_up.sh
@@ -9,6 +9,7 @@ if [[ $1 =~ "CE"* ]]
 then
 	index=${1:2:1}
 	#switch configuration - to modify MAC addresses
-	sudo ovs-ofctl add-flow tn_mac_ce$index -O OpenFlow13 in_port=2,actions=mod_dl_dst:00:10:02:00:00:0$index,output:1
+	sudo ovs-ofctl add-flow tn_mac_ce$index -O OpenFlow13 ip,nw_dst=10.2.$index.0/24,in_port=2,actions=mod_dl_dst:00:10:02:00:00:0$index,output:1
 	sudo ovs-ofctl add-flow tn_mac_ce$index -O OpenFlow13 in_port=1,actions=output:2
+	sudo ovs-ofctl add-flow tn_mac_ce$index -O OpenFlow13 ip,nw_dst=10.2.0.0/24,in_port=2,actions=output:1
 fi
